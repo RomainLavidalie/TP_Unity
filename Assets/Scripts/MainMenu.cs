7914c88514d4da426c216b1355bb943c7dca8173
Romain Lavidalie <83449831+RomainLavidalie@users.noreply.github.com>
2021-10-29 12:59:35 +0200

    main menu finished

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayCharacter()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PlayIsland()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void PlaySound()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void PlayController()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void PlayPatrick()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
